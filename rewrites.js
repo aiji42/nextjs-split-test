2780ad7ca7cedb39bdca5e8caade54ddf8c7bb0a
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-06-25 20:41:29 +0900

    wip

--- rewrites.js
+++ rewrites.js
@@ -4,8 +4,9 @@ const rewrites = async () => {
   if (process.env.VERCEL_ENV !== 'production') return {}
 
   // git branch -r --no-merge
-  const res = execSync('git branch -r')
-  const challengers = res.toString().match(/abtest_(.+)/g)
+  // const res = execSync('git branch -r')
+  // const challengers = res.toString().match(/abtest_(.+)/g)
+  const challengers = ['abtest_challenger']
   if (!challengers) return {}
 
   console.log(challengers)
