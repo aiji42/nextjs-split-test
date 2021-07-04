8b1fe27b4db3ff3b4f4cc2c7de026e4d2a437947
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-04 19:57:53 +0900

    update next-with-split and set abtest mapping

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 const { withSplit } = require('next-with-split')
 
 module.exports = withSplit({
+  splits: {
+    test1: {
+      path: '/foo/abtest/:path*',
+      hosts: {
+        original: 'https://nextjs-split-test-git-abtest-original-2021-07-04-aiji42.vercel.app',
+        challenger: 'https://nextjs-split-test-git-abtest-example-2021-07-04-aiji42.vercel.app'
+      }
+    },
+    test2: {
+      path: '/',
+      hosts: {
+        original: 'https://nextjs-split-test-git-abtest-original-2021-07-04-aiji42.vercel.app',
+        challenger: 'https://nextjs-split-test-git-abtest-example-2021-07-04-aiji42.vercel.app'
+      }
+    }
+  },
   reactStrictMode: true,
   rewrites: async () => ([
     {
