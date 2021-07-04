5e7b294c468ef7bfc515726ceaf8fa8519358173
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-04 19:15:24 +0900

    add next-with-split

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const withSplit = require('./next-with-split')
+const withSplit = require('next-with-split')
 
 module.exports = withSplit({
-  splits: {
-    branchMappings: {
-      abtest_challenger:
-        'https://nextjs-split-test-git-abtestchallenger-aiji42.vercel.app'
-    },
-    active: true
-  },
   reactStrictMode: true
 })
